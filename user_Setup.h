b901666ab5f5ee44da5553a5d44f2b816ce2d33c
koenll23 <smitk6355@gmail.com>
2025-02-03 09:31:45 +0100

    Update user_Setup.h
    language updated to english :)

--- user_Setup.h
+++ user_Setup.h
@@ -3,28 +3,31 @@
 #define USER_SETUP_INFO "User_Setup"
 #define ST7735_DRIVER  // Selecteer de ST7735 driver
 
-// Pin-configuratie
+// Pin-configuration of this project, make sure to match this
 #define TFT_MOSI 23    // SDA
 #define TFT_SCLK 18    // SCK
 #define TFT_CS   15    // CS
 #define TFT_DC   2     // AO
 #define TFT_RST  4     // RESET
+// gnd to gnd
+// vcc to 3.3v
+// LED / backlight to 3.3v
 
-// Displayresolutie
+// Displayresolution
 #define TFT_WIDTH  128
 #define TFT_HEIGHT 160
 
 
-// Kleurvolgorde
+// colour-order
 #define TFT_RGB_ORDER TFT_RGB
 
 
-// SPI-snelheid
-#define SPI_FREQUENCY  27000000  // 20 MHz
+// SPI-speed / frequency (27 max)
+#define SPI_FREQUENCY  27000000  // 27 MHz
 
 #define ST7735_REDTAB
 
-// Fonts activeren (zet in je User_Setup.h)
+// activating fonts
 #define LOAD_GLCD    // Standaard 6x8 pixels
 #define LOAD_FONT2   // Klein (5x7 pixels)
 #define LOAD_FONT4   // Middelgroot (6x8 pixels)
